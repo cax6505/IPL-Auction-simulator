e280a2b4130e05811eadaec527c2dce32e3c350b
CharanAdithya6505 <charanadithya6505@gmail.com>
2026-06-16 10:45:37 +0530

    refactor: delete legacy app/room folder structure

--- app/room/[roomCode]/layout.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { AuctionProvider } from "@/components/auction/AuctionContext";
-import React from "react";
-
-export default function RoomLayout({ children }: { children: React.ReactNode }) {
-  return <AuctionProvider>{children}</AuctionProvider>;
-}
