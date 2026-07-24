c58b3a0fbba0767ded564f7b643d37c6a7b0a1d1
CharanAdithya6505 <charanadithya6505@gmail.com>
2026-07-24 17:03:32 +0530

    feat(rooms): update room waiting lobby page logic

--- app/rooms/[code]/page.tsx
+++ app/rooms/[code]/page.tsx
@@ -30,8 +30,8 @@ export default function RoomLobbyPage() {
   }
 
   return (
-    <div className="min-h-screen bg-[#050505] text-slate-300 font-sans flex flex-col">
-      <main className="flex-1 max-w-5xl w-full mx-auto p-4 sm:p-6 lg:p-8 flex flex-col gap-6 pb-28 md:pb-8">
+    <div className="min-h-screen surface-0 text-slate-300 font-sans flex flex-col">
+      <main className="flex-1 max-w-7xl w-full mx-auto p-4 sm:p-6 lg:p-8 flex flex-col gap-6 pb-28 md:pb-8">
         <AuctionLobby />
       </main>
     </div>
